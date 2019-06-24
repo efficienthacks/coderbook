e46686e51797a2fa07e528ae293b33a48d170c77
efficienthacks <email@email.com>
2019-06-24 01:57:28 +0000

    Automated Release 31

--- docs/js/hugo-learn.js
+++ docs/js/hugo-learn.js
@@ -25,35 +25,35 @@ var getUrlParameter = function getUrlParameter(sPageURL) {
 // });
 
 // Change styles, depending on parameters set to the image
-images.each(function(index){
-  var image = $(this)
-  var o = getUrlParameter(image[0].src);
-  if (typeof o !== "undefined") {
-    var h = o["height"];
-    var w = o["width"];
-    var c = o["classes"];
-    image.css("width", function() {
-      if (typeof w !== "undefined") {
-        return w;
-      } else {
-        return "auto";
-      }
-    });
-    image.css("height", function() {
-      if (typeof h !== "undefined") {
-        return h;
-      } else {
-        return "auto";
-      }
-    });
-    if (typeof c !== "undefined") {
-      var classes = c.split(',');
-      for (i = 0; i < classes.length; i++) {
-        image.addClass(classes[i]);
-      }
-    }
-  }
-});
+// images.each(function(index){
+//   var image = $(this)
+//   var o = getUrlParameter(image[0].src);
+//   if (typeof o !== "undefined") {
+//     var h = o["height"];
+//     var w = o["width"];
+//     var c = o["classes"];
+//     image.css("width", function() {
+//       if (typeof w !== "undefined") {
+//         return w;
+//       } else {
+//         return "auto";
+//       }
+//     });
+//     image.css("height", function() {
+//       if (typeof h !== "undefined") {
+//         return h;
+//       } else {
+//         return "auto";
+//       }
+//     });
+//     if (typeof c !== "undefined") {
+//       var classes = c.split(',');
+//       for (i = 0; i < classes.length; i++) {
+//         image.addClass(classes[i]);
+//       }
+//     }
+//   }
+// });
 
 // Stick the top to the top of the screen when  scrolling
 $(document).ready(function(){
